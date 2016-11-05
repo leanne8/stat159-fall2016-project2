66a446417eb59569bfc8e69d9ee99ed04d6f5b0d
Leanne Lee <leannelee812@berkeley.edu>
2016-11-04 17:33:54 -0700

    ols done

---
+++ code/scripts/ols.R
@@ -0,0 +1,26 @@
+scaledCreditA = as.matrix(read.csv("data/scaled-credit.csv")) [,2:14]
+scaledCreditB = as.matrix(read.csv("data/scaled-credit.csv")) [,14]
+
+#Fit the ols regression 
+ols <- lm (scaledCreditB ~ scaledCreditA )
+
+#Find the summary of ols regression
+olsSum <- summary(ols)
+
+#Coeffiecients 
+olsCoef <- coef(ols)
+
+#MSE for OLS
+olsMSE <- mean(ols$residuals^2)
+
+#save into OLS binary file
+save(ols, olsSum, olsCoef, olsMSE, file="./data/ols.RData")
+
+sink('./data/ols-output.txt')
+cat('OLS summary statistics\n')
+print(olsSum)
+cat('OLS coefficients\n')
+print(olsCoef)
+cat('OLS MSE\n')
+print(olsMSE)
+sink()
